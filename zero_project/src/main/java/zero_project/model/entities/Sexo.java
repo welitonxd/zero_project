da52e047ec7ebc1e2b3379ce8359be7ece725052
Weliton <Weliton@PC>
2015-05-04 02:38:42 -0300

    Adição da entidade sexo

---
+++ zero_project/src/main/java/zero_project/model/entities/Sexo.java
@@ -0,0 +1,85 @@
+
+package zero_project.model.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import org.hibernate.annotations.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="sexo")
+
+public class Sexo implements Serializable {
+    
+    private static final long serialVersionUID =1L;
+    
+    @Id
+    @GeneratedValue
+    @Column(name="IdSexo",nullable=false)
+    private Integer idSexo;
+    @Column (name="Descricao", unique=true, nullable=false, length=9)
+    private String descricao;
+    
+    @OneToMany (mappedBy = "sexo", fetch =FetchType.LAZY)
+    @ForeignKey(name= "PessoaSexo")
+    private List<Pessoa> pessoas;
+
+    public Sexo() {
+    }
+
+    public Integer getIdSexo() {
+        return idSexo;
+    }
+
+    public void setIdSexo(Integer idSexo) {
+        this.idSexo = idSexo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(List<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + (this.idSexo != null ? this.idSexo.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Sexo other = (Sexo) obj;
+        if (this.idSexo != other.idSexo && (this.idSexo == null || !this.idSexo.equals(other.idSexo))) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
